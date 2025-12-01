2fc1da136c6f4d1587827ba4dcdeec8497892503
Joakim Nilsson <jojoje.nilsson@gmail.com>
2025-12-02 00:18:11 +0100

    no luck :(

--- dec1.py
+++ dec1.py
@@ -9,32 +9,33 @@ while 1:
 dailPosition = 50
 totalZeroPosition = 0
 for line in lines:
+    lastPosition = dailPosition
     isRightRotation = line[0] == 'R'
     number = int(line[1:])
 
     floor = abs(number // 100)
     if (isRightRotation):
         if dailPosition != 0 and dailPosition + number > 100:
-            print (f'Went from {dailPosition} to {dailPosition + number} counting up')
             totalZeroPosition += 1
             floor -= 1
 
         dailPosition = (dailPosition + number) % 100
     else:
         if dailPosition != 0 and dailPosition - number < 0:
-            print (f'Went from {dailPosition} to {dailPosition - number} counting up')
             totalZeroPosition += 1
             floor -= 1
 
         dailPosition = (dailPosition - number) % 100
     
     if dailPosition == 0:
-        print (f"Position is zero from instruction {line}")
         totalZeroPosition += 1
 
     if floor > 0:
-        print(f"Multipass {floor} times")
+        if number % 100 == 0 and dailPosition == 0:
+            floor -= 1
         totalZeroPosition += (floor)
-    
 
+    print(f'Current: {lastPosition}, instruction: {line}, new position: {dailPosition}, current zero count {totalZeroPosition}')
+
+    
 print (f'Total zero Position: {totalZeroPosition}')
